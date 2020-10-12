c9eadc91ec3c1f515e9f0b53650bfdf62146ee4b
DevHSA <harishsa85@gmail.com>
2020-10-12 07:25:03 +0530

    Contexts

--- chat-fe/src/components/NewContactModal.js
+++ chat-fe/src/components/NewContactModal.js
@@ -1,13 +1,23 @@
 import React, { useRef } from "react";
 import { Modal, Form, Button } from "react-bootstrap";
+import {useContacts} from '../context/ContactsProvider'
+
 
 export default function NewContactModal({ closeModal }) {
     const idRef = useRef();
     const nameRef = useRef();
+    const {createContact} = useContacts();
 
   function handleSubmit(e) {
 
     e.preventDefault()
+
+    
+    createContact(idRef.current.value, nameRef.current.value);
+
+    closeModal();
+
+
   }
 
   return (
